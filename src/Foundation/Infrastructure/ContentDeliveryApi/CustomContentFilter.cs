40afef52d607fbed5eec249751dc7af255e3f7c8
Cương Nguyễn <cuong0993@gmail.com>
2021-12-17 12:05:00 +0700

    Filter content delivery api

---
+++ src/Foundation/Infrastructure/ContentDeliveryApi/CustomContentFilter.cs
@@ -0,0 +1,16 @@
+﻿using EPiServer.ContentApi.Core.Serialization;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+
+namespace Foundation.Infrastructure.ContentDeliveryApi
+{
+    [ServiceConfiguration(typeof(IContentFilter), Lifecycle = ServiceInstanceScope.Singleton)]
+
+    internal class CustomContentFilter : ContentFilter<IContent>
+    {
+        public override void Filter(IContent content, ConverterContext converterContext)
+        {
+            // content.Name = null;
+        }
+    }
+}
